087e9a06b2179f83f96b40fa465870a51b920359
Colin Watson <cjwatson@debian.org>
2002-10-13 02:19:23 +0000

    src/debian: New directory for Debian work. To start with, add version 1.0.2 of binfmt-support (the earliest version I can still find).

---
+++ debian/init.d
@@ -0,0 +1,47 @@
+#! /bin/sh
+#
+# skeleton	example file to build /etc/init.d/ scripts.
+#		This file should be used to construct scripts for /etc/init.d.
+#
+#		Written by Miquel van Smoorenburg <miquels@cistron.nl>.
+#		Modified for Debian GNU/Linux
+#		by Ian Murdock <imurdock@gnu.ai.mit.edu>.
+#
+# Version:	@(#)skeleton  1.8  03-Mar-1998  miquels@cistron.nl
+#
+# This file was automatically customized by dh-make on Sun, 19 Mar 2000 23:06:38 +0000
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+NAME=binfmt-support
+DESC="additional executable binary formats"
+
+test -x $WRAPPER || exit 0
+
+set -e
+
+case "$1" in
+  start)
+    echo -n "Enabling $DESC: "
+	update-binfmts --enable
+    echo "$NAME."
+    ;;
+
+  stop)
+    echo -n "Disabling $DESC: "
+    update-binfmts --disable
+    echo "$NAME."
+    ;;
+
+  restart|force-reload)
+    $0 stop
+    $0 start
+    ;;
+
+  *)
+    N=/etc/init.d/$NAME
+    echo "Usage: $N {start|stop|restart|force-reload}" >&2
+    exit 1
+    ;;
+esac
+
+exit 0
---
+++ update-binfmts.pl
@@ -0,0 +1,447 @@
+#! /usr/bin/perl -w
+
+# Copyright (c) 2000 Colin Watson <cjw44@flatline.org.uk>.
+# See update-binfmts(8) for documentation.
+
+use strict;
+
+sub ENOENT () { 2; }
+
+my $VERSION = '1.0.2';
+
+my $errstarted = 0;
+my $test;
+my $admindir = '/var/lib/binfmts';
+my $package;
+my $mode;
+my ($name, $interpreter);
+my $type;
+my ($magic, $mask, $offset, $extension);
+
+local *BINFMT;
+
+sub wrap ($)
+{
+    my $text = shift;
+    $text =~ s/^(.{0,79})\s/$1\n/gm;
+    return $text;
+}
+
+sub quit ($;@)
+{
+    unless ($errstarted) { print STDERR "\n"; $errstarted = 1; }
+    print STDERR wrap "update-binfmts: @_\n";
+    exit 2;
+}
+
+sub version ()
+{
+    print "Debian GNU/Linux update-binfmts $VERSION.\n"
+	or quit "unable to write version message: $!";
+}
+
+sub usage ()
+{
+    version;
+    print <<EOF
+Copyright (c) 2000 Colin Watson. This is free software; see the GNU
+General Public License version 2 or later for copying conditions.
+
+Usage:
+
+  update-binfmts [options] --install <name> <path> <spec>
+  update-binfmts [options] --remove <name> <path>
+  update-binfmts [options] --display <name>
+  update-binfmts [options] --enable [<name>]
+  update-binfmts [options] --disable [<name>]
+
+  where <spec> is one of:
+
+    --magic <byte-sequence> [--mask <byte-sequence>] [--offset <offset>]
+    --extension <extension>
+
+Options:
+
+    --package <package-name>    for --install and --remove, specify the
+                                current package name
+    --admindir <directory>      use <directory> instead of /var/lib/binfmts
+                                as administration directory
+    --test                      don't do anything, just demonstrate
+    --help                      print this help screen and exit
+    --version                   output version and exit
+
+EOF
+	or quit "unable to write usage message: $!";
+}
+
+sub usage_quit ($;@)
+{
+    print STDERR wrap "update-binfmts: @_\n\n";
+    usage;
+    exit 2;
+}
+
+sub warning ($;@) {
+    unless ($errstarted) { print STDERR "\n"; $errstarted = 1; }
+    print STDERR wrap "update-binfmts: warning: @_\n";
+}
+
+sub check_modes ()
+{
+    return unless $mode;
+    usage_quit "two modes given: --$mode and $_";
+}
+
+sub check_types ()
+{
+    return unless $type;
+    usage_quit "two binary format specifications given: --$type and $_";
+}
+
+sub print_binfmt ($)
+{
+    my $line = shift;
+    if ($line =~ /\n/)
+    {
+	quit "newlines prohibited in update-binfmts files ($line).";
+    }
+    print BINFMT "$line\n";
+}
+
+sub rename_mv ($$)
+{
+    my ($source, $dest) = @_;
+    return (rename($source, $dest) || (system('mv', $source, $dest) == 0));
+}
+
+sub get_binfmt ($)
+{
+    my $name = shift;
+    my @binfmt;
+    open BINFMT, "$admindir/$name"
+	or quit "unable to open $admindir/$name: $!";
+    push @binfmt, scalar <BINFMT> for 0..5;
+    close BINFMT;
+    chomp @binfmt;
+    return @binfmt;
+}
+
+my $procdir = '/proc/sys/fs/binfmt_misc';
+my $register = "$procdir/register";
+
+sub enable (;$);
+sub enable (;$)
+{
+    my $name = shift;
+    unless (-d $procdir)
+    {
+	if (system('/sbin/modprobe', 'binfmt_misc'))
+	{
+	    warning "Couldn't load the binfmt_misc module.";
+	    return 0;
+	}
+	else
+	{
+	    unless (-d $procdir)
+	    {
+		warning "binfmt_misc module seemed to load, but no $procdir " .
+			"directory! Giving up.";
+		return 0;
+	    }
+	}
+    }
+    if (defined $name)
+    {
+	unless (-e "$admindir/$name")
+	{
+	    warning "$name not in database of installed binary formats.";
+	    return 0;
+	}
+	my ($package, $type, $offset, $magic, $mask, $interpreter) =
+	    get_binfmt $name;
+	$type = ($type eq 'magic') ? 'M' : 'E';
+	my $regstring = ":$name:$type:$offset:$magic:$mask:$interpreter:\n";
+	if ($test)
+	{
+	    print "\nenable $name with format string:\n$regstring\n";
+	}
+	else
+	{
+	    open REGISTER, ">$register"
+		or warning "unable to open $register for writing: $!",
+		   return 0;
+	    print REGISTER $regstring;
+	    close REGISTER
+		or warning "unable to close $register: $!", return 0;
+	}
+    }
+    else
+    {
+	opendir ADMINDIR, $admindir
+	    or warning "unable to open $admindir: $!", return 0;
+	while (defined($_ = readdir ADMINDIR))
+	{
+	    enable $_ unless /^\.\.?$/;
+	}
+	closedir ADMINDIR;
+    }
+    return 1;
+}
+
+sub disable (;$);
+sub disable (;$)
+{
+    my $name = shift;
+    return 1 unless -d $procdir;    # We're disabling anyway, so we don't mind
+    if (defined $name)
+    {
+	unless (-e "$admindir/$name")
+	{
+	    warning "$name not in database of installed binary formats.";
+	    return 0;
+	}
+	unless (-e "$procdir/$name")
+	{
+	    warning "$name binary format not enabled.";
+	    return 0;
+	}
+
+	open PROCENTRY, "$procdir/$name"
+	    or warning "unable to open $procdir/$name: $!", return 0;
+	my ($package, $type, $offset, $magic, $mask, $interpreter) =
+	    get_binfmt $name;
+	my $cur_interpreter;
+	while (<PROCENTRY>)
+	{
+	    if (/^interpreter (.*)/)
+	    {
+		$cur_interpreter = $1;
+		last;
+	    }
+	}
+	close PROCENTRY;
+	unless (defined $cur_interpreter)
+	{
+	    warning "$procdir/$name doesn't seem to be a binary format " .
+		    "registration.";
+	    return 0;
+	}
+	unless ($cur_interpreter eq $interpreter)
+	{
+	    warning "$procdir/$name set by system administrator to " .
+		    "$cur_interpreter rather than $interpreter.";
+	    return 0;
+	}
+
+	if ($test)
+	{
+	    print "\ndisable $name\n";
+	}
+	else
+	{
+	    open PROCENTRY, ">$procdir/$name"
+		or warning "unable to open $procdir/$name for writing: $!",
+		   return 0;
+	    print PROCENTRY -1;
+	    close PROCENTRY
+		or warning "unable to close $procdir/$name: $!", return 0;
+	    -e "$procdir/$name"
+		and quit "removal of $procdir/$name ignored by kernel!";
+	}
+    }
+    else
+    {
+	opendir ADMINDIR, $admindir
+	    or warning "unable to open $admindir: $!", return 0;
+	while (defined($_ = readdir ADMINDIR))
+	{
+	    disable $_ unless /^\.\.?$/;
+	}
+	closedir ADMINDIR;
+    }
+    return 1;
+}
+
+while (defined($_ = shift))
+{
+    last if /^--$/;
+    if (!/^--/)		    { usage_quit "unknown argument \'$_'"; }
+    elsif (/^--help$/)	    { usage; exit 0; }
+    elsif (/^--version$/)   { version; exit 0; }
+    elsif (/^--test$/)	    { $test = 1; }
+    elsif (/^--admindir$/)
+    {
+	usage_quit "--admindir needs <path>" unless @ARGV >= 1;
+	$admindir = shift;
+    }
+
+    # Main modes of operation.
+    elsif (/^--(install|remove)$/)
+    {
+	check_modes;
+	usage_quit "--$1 needs <name> <path>" unless @ARGV >= 2;
+	$name = shift;
+	$interpreter = shift;
+	-e $interpreter or
+	    warning "$interpreter not found, but continuing anyway as you " .
+		    "request";
+	$mode = $1;
+    }
+    elsif (/^--display$/)
+    {
+	check_modes;
+	usage_quit '--display needs <name>' unless @ARGV >= 1;
+	$name = shift;
+	$mode = 'display';
+    }
+    elsif (/^--(enable|disable)$/)
+    {
+	check_modes;
+	$name = (@ARGV >= 1) ? shift : undef;
+	$mode = $1;
+    }
+
+    # Package name.
+    elsif (/^--package$/)
+    {
+	usage_quit 'more than one --package option given' if defined $package;
+	usage_quit '--package needs <package-name>' unless @ARGV >= 1;
+	$package = shift;
+    }
+
+    # Binary format specifications.
+    elsif (/^--magic$/)
+    {
+	check_types;
+	usage_quit '--magic needs <byte-sequence>' unless @ARGV >= 1;
+	$magic = shift;
+	$type = 'magic';
+    }
+    elsif (/^--extension$/)
+    {
+	check_types;
+	usage_quit '--extension needs <extension>' unless @ARGV >= 1;
+	$extension = shift;
+	$type = 'extension';
+    }
+
+    # --magic options.
+    elsif (/^--mask$/)
+    {
+	usage_quit 'more than one --mask option given' if defined $mask;
+	usage_quit '--mask needs <byte-sequence>' unless @ARGV >= 1;
+	$mask = shift;
+    }
+    elsif (/^--offset$/)
+    {
+	usage_quit 'more than one --offset option given' if defined $offset;
+	usage_quit '--offset needs <offset>' unless @ARGV >= 1;
+	$offset = shift;
+	$offset =~ /^\d+$/ or usage_quit 'offset must be a whole number';
+    }
+
+    else { usage_quit "unknown argument \'$_'"; }
+}
+
+$package = ':' unless defined $package;
+
+unless (defined $mode)
+{
+    usage_quit 'you must use one of --install, --remove, --display, ' .
+	       '--enable, or --disable';
+}
+
+if ($mode eq 'install')
+{
+    defined $type or usage_quit '--install requires a <spec> option';
+    if ($type eq 'extension')
+    {
+	defined $magic
+	    and usage_quit "can't use both --magic and --extension";
+	defined $mask	and usage_quit "can't use --mask with --extension";
+	defined $offset	and usage_quit "can't use --offset with --extension";
+    }
+    if ($name =~ /^(\.\.?|register|status)$/)
+    {
+	usage_quit "binary format name $name is reserved";
+    }
+}
+
+-d $admindir or quit "unable to open $admindir: $!";
+
+if ($mode eq 'install')
+{
+    if (-f "$admindir/$name")
+    {
+	warning "$admindir/$name already exists; overriding it";
+    }
+    if ($test)
+    {
+	print "installing the following binary format description:\n";
+	open BINFMT, '>&1' or quit "unable to dup standard output: $!";
+    }
+    else
+    {
+	unlink "$admindir/$name.tmp" or $! == ENOENT
+	    or quit "unable to ensure $admindir/$name.tmp nonexistent: $!";
+	open BINFMT, ">$admindir/$name.tmp"
+	    or quit "unable to open $admindir/$name.tmp for writing: $!";
+    }
+    print_binfmt $package;
+    print_binfmt $type;
+    print_binfmt (defined($offset) ? $offset : '');
+    print_binfmt (defined($magic) ? $magic : $extension);
+    print_binfmt (defined($mask) ? $mask : '');
+    print_binfmt $interpreter;
+    if ($test)
+    {
+	close BINFMT or quit "unable to close duplicated standard output: $!";
+    }
+    else
+    {
+	close BINFMT or quit "unable to close $admindir/$name.tmp: $!";
+	rename_mv "$admindir/$name.tmp", "$admindir/$name"
+	    or quit "unable to install $admindir/$name.tmp as " .
+		    "$admindir/$name: $!";
+    }
+    enable $name or quit "unable to enable binary format $name.";
+}
+
+elsif ($mode eq 'remove')
+{
+    unless (-f "$admindir/$name")
+    {
+	quit "$admindir/$name does not exist; nothing to do!";
+    }
+    my $oldpackage = (get_binfmt $name)[0];
+    unless ($package eq $oldpackage)
+    {
+	$package = '<local>'	    if $package eq ':';
+	$oldpackage = '<local>'	    if $oldpackage eq ':';
+	quit "current package is $package, but binary format already " .
+	     "installed by $oldpackage.";
+    }
+    disable $name or quit "unable to disable binary format $name.";
+    if ($test)
+    {
+	print "removing $admindir/$name\n";
+    }
+    else
+    {
+	unlink "$admindir/$name"
+	    or quit "unable to remove $admindir/$name: $!";
+    }
+}
+
+elsif ($mode eq 'display')
+{
+    open BINFMT, "$admindir/$name"
+	or quit "$name binary format not installed.";
+    local $/ = undef;
+    print <BINFMT>;
+    close BINFMT;
+}
+
+elsif ($mode eq 'enable')	{ enable $name; }
+elsif ($mode eq 'disable')	{ disable $name; }
+
